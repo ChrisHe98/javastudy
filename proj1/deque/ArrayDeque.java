43850c91a3eee9151918a523e7af445effbd3844
Chris Xu <junhuixu98@gmail.com>
2024-01-13 22:00:34 -0800

    updated again for indentation

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -29,168 +29,168 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     public Iterator<T> iterator() {
         return new ArrayIterator();
     }
-        private T[] items;
-        private int size;
-        private int nextFirst;
-        private int nextLast;
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
 
-        public ArrayDeque() {
-            items = (T[]) new Object[8];
-            size = 0;
-            nextFirst = 4;
-            nextLast = 5;
-        }
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
 
         /**
          * Resizes the underlying array to the target capacity.
          */
-        private void resize(int capacity) {
-            T[] a = (T[]) new Object[capacity];
-            if (size == 0) {
-                System.arraycopy(items, 0, a, 0, size);
-            } else if (getLastIndex() >= getFirstIndex()) {
-                System.arraycopy(items, getFirstIndex(), a, 0, size);
-            } else {
-                System.arraycopy(items, getFirstIndex(), a, 0, items.length - getFirstIndex());
-                System.arraycopy(items, 0, a, items.length - getFirstIndex(), getLastIndex() + 1);
-            }
-            int sizeTemp = size;
-            items = a;
-            size = sizeTemp;
-            nextFirst = items.length - 1;
-            nextLast = size;
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        if (size == 0) {
+            System.arraycopy(items, 0, a, 0, size);
+        } else if (getLastIndex() >= getFirstIndex()) {
+            System.arraycopy(items, getFirstIndex(), a, 0, size);
+        } else {
+            System.arraycopy(items, getFirstIndex(), a, 0, items.length - getFirstIndex());
+            System.arraycopy(items, 0, a, items.length - getFirstIndex(), getLastIndex() + 1);
         }
+        int sizeTemp = size;
+        items = a;
+        size = sizeTemp;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
 
         /**
          * Adds x to the front of the list.
          */
-        public void addFirst(T x) {
-            if (size == items.length) {
-                resize(size * 2);
-            }
-            items[nextFirst] = x;
-            if (nextFirst == 0) {
-                nextFirst = items.length - 1;
-            } else {
-                nextFirst--;
-            }
-            size++;
+    public void addFirst(T x) {
+        if (size == items.length) {
+            resize(size * 2);
         }
+        items[nextFirst] = x;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst--;
+        }
+        size++;
+    }
 
         /**
          * Adds x to the end of the list.
          */
-        public void addLast(T x) {
-            if (size == items.length) {
-                resize(size * 2);
-            }
-            items[nextLast] = x;
-            if (nextLast == items.length - 1) {
-                nextLast = 0;
-            } else {
-                nextLast++;
-            }
-            size++;
+    public void addLast(T x) {
+        if (size == items.length) {
+            resize(size * 2);
         }
+        items[nextLast] = x;
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        } else {
+            nextLast++;
+        }
+        size++;
+    }
 
 
         /**
          * Returns the size of the list.
          */
-        public int size() {
-            return size;
-        }
+    public int size() {
+        return size;
+    }
 
-        public void printDeque() {
-            if (size == 0) {
-                System.out.println();
-            }
-            for (T item : items) {
-                System.out.print(items + " ");
-            }
+    public void printDeque() {
+        if (size == 0) {
             System.out.println();
         }
-
-        public T removeFirst() {
-            if (size == 0) {
-                return null;
-            }
-            nextFirst = getFirstIndex();
-            T firstItem = items[nextFirst];
-            items[nextFirst] = null;
-            size--;
-            if ((size < (double) items.length / 4) && (items.length >= 16)) {
-                resize(items.length / 2);
-            }
-            return firstItem;
+        for (T item : items) {
+            System.out.print(items + " ");
         }
+        System.out.println();
+    }
 
-        private int getFirstIndex() {
-            if (size == 0) {
-                throw new NoSuchElementException("No more items");
-            }
-            int firstIndex;
-            if (nextFirst == items.length - 1) {
-                firstIndex = 0;
-            } else {
-                firstIndex = nextFirst + 1;
-            }
-            return firstIndex;
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
         }
-
-        public T removeLast() {
-            if (size == 0) {
-                return null;
-            }
-            nextLast = getLastIndex();
-            T lastItem = items[nextLast];
-            items[nextLast] = null;  // We've already adjusted nextLast, so this is safe.
-            size--;
-            if ((size < (double) items.length / 4) && (items.length >= 16)) {
-                resize(items.length / 2);
-            }
-            return lastItem;
+        nextFirst = getFirstIndex();
+        T firstItem = items[nextFirst];
+        items[nextFirst] = null;
+        size--;
+        if ((size < (double) items.length / 4) && (items.length >= 16)) {
+            resize(items.length / 2);
         }
+        return firstItem;
+    }
 
-        private int getLastIndex() {
-            if (size == 0) {
-                throw new NoSuchElementException("No more items");
-            }
-            int lastIndex;
-            if (nextLast == 0) {
-                lastIndex = items.length - 1;
-            } else {
-                lastIndex = nextLast - 1;
-            }
-            return lastIndex;
+    private int getFirstIndex() {
+        if (size == 0) {
+            return nextFirst;
         }
-
-        public T get(int index) {
-            if (index < 0 || index >= size) {
-                return null;
-            }
-            return items[(getFirstIndex() + index) % items.length];
+        int firstIndex;
+        if (nextFirst == items.length - 1) {
+            firstIndex = 0;
+        } else {
+            firstIndex = nextFirst + 1;
         }
+        return firstIndex;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        nextLast = getLastIndex();
+        T lastItem = items[nextLast];
+        items[nextLast] = null;  // We've already adjusted nextLast, so this is safe.
+        size--;
+        if ((size < (double) items.length / 4) && (items.length >= 16)) {
+            resize(items.length / 2);
+        }
+        return lastItem;
+    }
+
+    private int getLastIndex() {
+        if (size == 0) {
+            return nextLast;
+        }
+        int lastIndex;
+        if (nextLast == 0) {
+            lastIndex = items.length - 1;
+        } else {
+            lastIndex = nextLast - 1;
+        }
+        return lastIndex;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return items[(getFirstIndex() + index) % items.length];
+    }
 
 
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Deque)) {
-                return false;
-            }
-            Deque<T> otherDeque = (Deque<T>) o;
-            if (this.size() != otherDeque.size()) {
-                return false;
-            }
-            for (int i = 0; i < this.size(); i++) {
-                if (!this.get(i).equals(otherDeque.get(i))) {
-                    return false;
-                }
-            }
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-
-
+        if (!(o instanceof Deque)) {
+            return false;
+        }
+        Deque<T> otherDeque = (Deque<T>) o;
+        if (this.size() != otherDeque.size()) {
+            return false;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (!this.get(i).equals(otherDeque.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
+
+
+}
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -2,107 +2,171 @@ package deque;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Iterator;
 
+/** ArrayDeque test. */
 public class ArrayDequeTest {
+
+    /** Test add functionality*/
     @Test
-    /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
-    public void addRemoveTest() {
+    public void addTest(){
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        assertTrue(ad1.isEmpty());
 
-        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
-        // should be empty
-        assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
+        ad1.addLast(3);
+        assertFalse(ad1.isEmpty());
 
-        lld1.addFirst(10);
-        // should not be empty
-        assertFalse("lld1 should contain 1 item", lld1.isEmpty());
+        ad1.addFirst(4);
+        assertFalse(ad1.isEmpty());
+        assertEquals(2, ad1.size());
 
-        lld1.removeFirst();
-        // should be empty
-        assertTrue("lld1 should be empty after removal", lld1.isEmpty());
+        ad1.printDeque();
+    }
+
+
+    @Test
+    public void addRemoveTest(){
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+        assertTrue(ad1.isEmpty());
+
+        ad1.addFirst("is");
+        ad1.addFirst("this");
+        ad1.addLast("a");
+        ad1.addLast("dog");
+
+        assertEquals(4, ad1.size());
+        ad1.printDeque();
+
+        assertEquals("dog", ad1.removeLast());
+        assertEquals("this", ad1.removeFirst());
+
+        assertEquals(2, ad1.size());
+
+        ad1.removeLast();
+        ad1.removeLast();
+
+        assertNull(ad1.removeLast());
+        assertNull(ad1.removeFirst());
+    }
+
+    @Test
+    public void getTest(){
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+
+        ad1.addLast("this");
+        ad1.addLast("is");
+        ad1.addLast("a");
+        ad1.addLast("dog");
+
+        assertEquals("this", ad1.get(0));
+        assertEquals("dog", ad1.get(3));
+
+        assertNull(ad1.get(4));
+    }
+
+    @Test
+    public void resizeTest(){
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        ArrayDeque<Integer> ad2 = new ArrayDeque<Integer>();
+
+        int targetSize = 1000;
+        for (int i = 0; i < targetSize; i++){
+            ad1.addLast(i);
+        }
+        for (int i = 0; i < targetSize; i++){
+            ad2.addFirst(i);
+        }
+        assertEquals(targetSize, ad1.size());
+        assertEquals(targetSize, ad2.size());
+
+        int getItem1 = ad1.get(78);
+        assertEquals(78, getItem1);
+
+        int getItem2 = ad2.get(0);
+        assertEquals(targetSize - 1, getItem2);
+
+        assertNull(ad1.get(targetSize));
 
     }
 
     @Test
-    /* Tests removing from an empty deque */
-    public void removeEmptyTest() {
-
-
-        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
-        lld1.addFirst(3);
-
-        lld1.removeLast();
-        lld1.removeFirst();
-        lld1.removeLast();
-        lld1.removeFirst();
-
-        int size = lld1.size();
-        String errorMsg = "  Bad size returned when removing from empty deque.\n";
-        errorMsg += "  student size() returned " + size + "\n";
-        errorMsg += "  actual size() returned 0\n";
-
-        assertEquals(errorMsg, 0, size);
-
-    }
-
-    @Test
-    /* Check if you can create LinkedListDeques with different parameterized types*/
-    public void multipleParamTest() {
-
-        ArrayDeque<String> lld1 = new ArrayDeque<String>();
-        ArrayDeque<Double> lld2 = new ArrayDeque<>();
-        ArrayDeque<Boolean> lld3 = new ArrayDeque<>();
-
-
-        lld1.addFirst("string");
-        lld2.addFirst(3.14159);
-        lld3.addFirst(true);
-
-        String s = lld1.removeFirst();
-        double d = lld2.removeFirst();
-        boolean b = lld3.removeFirst();
-
-    }
-
-    @Test
-    /* check if null is return when removing from an empty LinkedListDeque. */
-    public void emptyNullReturnTest() {
-        ArrayDeque<String> lld1 = new ArrayDeque<String>();
-
-        boolean passed1 = false;
-        boolean passed2 = false;
-        assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
-        assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
-
-
-    }
-
-    @Test
-    /* Add large number of elements to deque; check if order is correct. */
-    public void bigADequeTest() {
-
-        ArrayDeque<Integer> lld1 = new ArrayDeque<>();
-        for (int i = 0; i < 1000; i++) {
-            lld1.addLast(i);
+    public void iteratorTest(){
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        for (int i = 0; i < 1000; i++){
+            ad1.addLast(i);
         }
 
-        for (double i = 0; i < 500; i++) {
-            assertEquals("Should have the same value", i, (double) lld1.removeFirst(), 0.0);
+        Iterator<Integer> iter = ad1.iterator();
+        int exp = 0;
+        while (iter.hasNext()){
+            int act = iter.next();
+            assertEquals(exp, act);
+            exp += 1;
         }
-
-        for (double i = 999; i > 500; i--) {
-            assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
-        }
-
-
     }
+
     @Test
-    public void getFunctionTest(){
-        ArrayDeque<Integer> lld1 = new ArrayDeque<>();
-        lld1.addFirst(3);
-        lld1.addFirst(4);
-        lld1.addLast(5); //4 3 5
-        int z = lld1.get(2);
-        assertEquals(5, z);
+    public void equalTest(){
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        ArrayDeque<Integer> ad2 = new ArrayDeque<Integer>();
+
+        int targetSize = 1000;
+        for (int i = 0; i < targetSize; i++){
+            ad1.addLast(i);
+            ad2.addLast(i);
+        }
+        assertTrue(ad1.equals(ad2));
+
+        assertFalse(ad1.equals(30));
+
+        ArrayDeque<String> ad3 = new ArrayDeque<>();
+        ad3.addLast("aaa");
+        assertFalse(ad1.equals(ad3));
+    }
+
+    @Test
+    public void randomTest(){
+        ArrayDeque<Integer> ad = new ArrayDeque<Integer>();
+        LinkedListDeque<Integer> lld = new LinkedListDeque<Integer>();
+
+        int N = 50000;
+        for (int i = 0 ; i < N; i++){
+            int operationNumber = StdRandom.uniform(0, 6);
+
+            switch (operationNumber){
+                case 0:
+                    // addLast the same value to both lists
+                    int randVal1 = StdRandom.uniform(0, 1000);
+                    ad.addLast(randVal1);
+                    lld.addLast(randVal1);
+
+                case 1:
+                    // addFirst the same value to both lists
+                    int randVal2 = StdRandom.uniform(0, 100);
+                    ad.addFirst(randVal2);
+                    lld.addFirst(randVal2);
+
+                case 2:
+                    // assert if size are equal
+                    assertEquals(lld.size(), ad.size());
+
+                case 3:
+                    // assert if get random index value is equal
+                    if (ad.size() > 0 && lld.size() > 0) {
+                        int randIndex = StdRandom.uniform(0, ad.size());
+                        assertEquals(lld.get(randIndex), ad.get(randIndex));
+                    }
+
+                case 4:
+                    // assert if removeLast is equal
+                    assertEquals(lld.removeLast(), ad.removeLast());
+
+                case 5:
+                    // assert if removeFirst is equal
+                    assertEquals(lld.removeFirst(), ad.removeFirst());
+            }
+        }
     }
 }
